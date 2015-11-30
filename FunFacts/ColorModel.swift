3e6aa2591d916082cab7dbebcf8e344fd33b1eac
hassan <hassan.talat.92@gmail.com>
2015-11-29 17:26:52 -0800

    Finishing touches to the course

---
+++ FunFacts/ColorModel.swift
@@ -0,0 +1,9 @@
+//
+//  ColorModel.swift
+//  FunFacts
+//
+//  Created by Hassan Talat on 11/29/15.
+//  Copyright © 2015 Hassan Talat. All rights reserved.
+//
+
+import Foundation
---
+++ FunFacts/FactModel.swift
@@ -0,0 +1,9 @@
+//
+//  FactModel.swift
+//  FunFacts
+//
+//  Created by Hassan Talat on 11/28/15.
+//  Copyright © 2015 Hassan Talat. All rights reserved.
+//
+
+import Foundation
